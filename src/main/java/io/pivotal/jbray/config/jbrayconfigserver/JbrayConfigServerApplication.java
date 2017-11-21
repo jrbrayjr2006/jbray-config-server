81955ba7c4607a664fb299e02d3ad3d0efe95f07
James Bray <jbray@Jamess-MacBook-Pro.local>
2017-11-21 15:49:22 -0600

    Created additional configuration local configuration files in YAML format and modified the JbrayConfigServerApplication class

--- src/main/java/io/pivotal/jbray/config/jbrayconfigserver/JbrayConfigServerApplication.java
+++ src/main/java/io/pivotal/jbray/config/jbrayconfigserver/JbrayConfigServerApplication.java
@@ -1,5 +1,6 @@
 package io.pivotal.jbray.config.jbrayconfigserver;
 
+//import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -7,6 +8,11 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
 public class JbrayConfigServerApplication {
+
+	//@Value("${configuration.name}")
+	void setName(String name) {
+		System.out.println("setting the name:  " + name);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JbrayConfigServerApplication.class, args);
